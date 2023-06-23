32bacca266162f10458f77a301bdc3acea58b469
Renan Araújo <awkrenan@gmail.com>
2023-06-22 23:42:59 -0300

    added simple permission logic

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	permissionGranted bool
 )
 
 func main() {
+	// Set initial permission status to false
+	permissionGranted = true
+
+	// Create a new Gin router
 	router := gin.Default()
 
+	// Define your API routes
 	router.GET("/users", handleUserRequest)
+	router.GET("/admin/approve", handleAdminApproval)
 
+	// Start the server
 	router.Run(":8080")
 }
 
 func handleUserRequest(c *gin.Context) {
-	permissionGranted := checkPermission()
-
 	if permissionGranted {
+		// Forward the request to the Node.js API
 		http.Redirect(
 			c.Writer,
 			c.Request,
@@ -24,12 +36,20 @@ func handleUserRequest(c *gin.Context) {
 			http.StatusTemporaryRedirect,
 		)
 	} else {
+		// Respond with an error message or denial
 		c.JSON(http.StatusForbidden, gin.H{
-			"Error": "Permission denied",
+			"error": "Permission denied",
 		})
 	}
 }
 
-func checkPermission() bool {
-	return false
+func handleAdminApproval(c *gin.Context) {
+	// Set the permission status to true upon manual approval
+	permissionGranted = true
+
+	fmt.Println("Permission granted manually by the administrator")
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Permission granted",
+	})
 }
